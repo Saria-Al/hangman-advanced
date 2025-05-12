0a822a4008065ce1e0c79ceaf687a901f2c0d7cd
Sar Al <saral@alMacBook-Pro-alkhas-bSar.local>
2025-05-12 08:44:47 +0300

    Fix session logic and ensure @game object is always defined

--- main.rb
+++ main.rb
@@ -1,136 +1,80 @@
 require 'sinatra'
-require 'sinatra/base'
-require 'sinatra/reloader' if development?
+require './lib/hangperson_game'  # تأكد من أن هذا المسار صحيح
 require 'json'
-require_relative 'game'  # تأكد أن لديك ملف game.rb
 
-class HangmanApp < Sinatra::Base
-  configure do
-    set :public_folder, File.expand_path('../public', __FILE__)
-    set :views, File.expand_path('../views', __FILE__)
-    set :bind, '0.0.0.0'
-    set :port, ENV['PORT'] || 4567
-    set :environment, :production
+use Rack::Session::Cookie, secret: 'a_very_secure_super_long_key_that_is_definitely_more_than_sixty_four_characters_long_1234567890'
+
+helpers do
+  def get_game
+    session[:game] ||= HangpersonGame.new('')
   end
 
-  WORDS = %w[
-    apple banana orange lemon mango peach pear grape melon kiwi
-    rocket planet jungle button camera garden window mirror pillow remote
-    python diamond cyclone pyramid blanket zephyr lantern mystery thunder whisper
-  ]
+  def sarcastic_remark
+    [
+      "Seriously?", "Try harder!", "You're killing me!",
+      "Is this guessing or wishing?", "Are you even trying?"
+    ].sample
+  end
 
-  HINTS = {
-    'apple' => 'A common red or green fruit.',
-    'banana' => 'A long curved yellow fruit.',
-    'orange' => 'A round citrus fruit and a color.',
-    'lemon' => 'A sour yellow fruit.',
-    'mango' => 'A sweet tropical fruit.',
-    'peach' => 'A soft fruit with fuzzy skin.',
-    'pear' => 'A green fruit shaped like a teardrop.',
-    'grape' => 'Small purple or green fruit, often in bunches.',
-    'melon' => 'A large juicy fruit with seeds.',
-    'kiwi' => 'A brown fuzzy fruit with green inside.',
-    'rocket' => 'Used to launch into space.',
-    'planet' => 'A celestial body orbiting a star.',
-    'jungle' => 'A dense forest with wild animals.',
-    'button' => 'Pressed to control something.',
-    'camera' => 'Used to take pictures.',
-    'garden' => 'A place where plants and flowers grow.',
-    'window' => 'An opening in a wall to let light in.',
-    'mirror' => 'You see your reflection in it.',
-    'pillow' => 'You rest your head on it while sleeping.',
-    'remote' => 'Used to control a TV from a distance.',
-    'python' => 'A large snake or a programming language.',
-    'diamond' => 'A precious stone, very hard.',
-    'cyclone' => 'A violent rotating windstorm.',
-    'pyramid' => 'A triangle-shaped structure in Egypt.',
-    'blanket' => 'Used to keep you warm in bed.',
-    'zephyr' => 'A soft gentle breeze.',
-    'lantern' => 'A portable light, often with a handle.',
-    'mystery' => 'Something unexplained or puzzling.',
-    'thunder' => 'Loud sound after lightning.',
-    'whisper' => 'To speak very softly.'
-  }
+  def hangman_ascii(wrong_guesses)
+    stages = [
+      "<pre>  +---+\n      |\n      |\n      |\n     ===</pre>",
+      "<pre>  +---+\n  O   |\n      |\n      |\n     ===</pre>",
+      "<pre>  +---+\n  O   |\n  |   |\n      |\n     ===</pre>",
+      "<pre>  +---+\n  O   |\n /|   |\n      |\n     ===</pre>",
+      "<pre>  +---+\n  O   |\n /|\\  |\n      |\n     ===</pre>",
+      "<pre>  +---+\n  O   |\n /|\\  |\n /    |\n     ===</pre>",
+      "<pre>  +---+\n  O   |\n /|\\  |\n / \\  |\n     ===</pre>"
+    ]
+    stages[[wrong_guesses, stages.size - 1].min]
+  end
 
-  enable :sessions
-
-  helpers do
-    def sarcastic_remark
-      [
-        "Seriously?", "Try harder!", "You're killing me!",
-        "Is this guessing or wishing?", "Are you even trying?"
-      ].sample
-    end
-
-    def hangman_ascii(wrong_guesses)
-      stages = [
-        "<pre>  +---+\n      |\n      |\n      |\n     ===</pre>",
-        "<pre>  +---+\n  O   |\n      |\n      |\n     ===</pre>",
-        "<pre>  +---+\n  O   |\n  |   |\n      |\n     ===</pre>",
-        "<pre>  +---+\n  O   |\n /|   |\n      |\n     ===</pre>",
-        "<pre>  +---+\n  O   |\n /|\\  |\n      |\n     ===</pre>",
-        "<pre>  +---+\n  O   |\n /|\\  |\n /    |\n     ===</pre>",
-        "<pre>  +---+\n  O   |\n /|\\  |\n / \\  |\n     ===</pre>"
-      ]
-      stages[[wrong_guesses, stages.size - 1].min]
-    end
-
-    def game_won?
-      return false if session[:word].nil? || session[:guesses].nil?
-      (session[:word].chars - session[:guesses]).empty?
-    end
-
-    def game_lost?
-      session[:wrong_guesses] >= 6
-    end
-
-    def generate_hint(word, guesses)
-      if session[:level] && session[:level] >= 3
-        "The word looks like: " + word.chars.map { |c| guesses.include?(c) ? c : '_' }.join
-      else
-        "The word starts with: #{word[0]}"
-      end
+  def generate_hint(word, guesses)
+    if session[:level] && session[:level] >= 3
+      "The word looks like: " + word.chars.map { |c| guesses.include?(c) ? c : '_' }.join
+    else
+      "The word starts with: #{word[0]}"
     end
   end
-
-  get '/' do
-    session.clear
-    redirect '/new'
-  end
-
-  get '/new' do
-    word = WORDS.sample
-    session[:word] = word
-    session[:guesses] = []
-    session[:wrong_guesses] = 0
-    session[:hint] = nil
-    session[:level] ||= 1
-    erb :index
-  end
-
-  post '/guess' do
-    letter = params[:letter].downcase
-    unless session[:guesses].include?(letter)
-      session[:guesses] << letter
-      session[:wrong_guesses] += 1 unless session[:word].include?(letter)
-    end
-
-    session[:level] += 1 if game_won?
-    redirect '/game'
-  end
-
-  get '/game' do
-    @game = Game.new(session[:word] || "default", session[:guesses] || [])
-    @game_won = game_won?
-    @game_lost = game_lost?
-    @sarcasm = sarcastic_remark if !@game_won && !@game_lost && session[:wrong_guesses] > 0
-    erb :index
-  end
-
-  post '/hint' do
-    session[:hint] = generate_hint(session[:word], session[:guesses])
-    redirect back
-  end
-
-  run! if __FILE__ == $PROGRAM_NAME
 end
+
+get '/' do
+  game = get_game
+  @word = game.word
+  @guesses = game.guesses
+  @wrong_guesses = game.wrong_guesses
+  @ascii = hangman_ascii(game.wrong_guesses)
+  @game_won = game.won?
+  @game_lost = game.lost?
+  @level = session[:level] || 1
+  @hint = session[:hint]
+  @sarcasm = sarcastic_remark if !@game_won && !@game_lost && game.wrong_guesses > 0
+  erb :index
+end
+
+post '/new' do
+  word = HangpersonGame.get_random_word
+  session[:game] = HangpersonGame.new(word)
+  session[:hint] = nil
+  session[:level] ||= 1
+  redirect '/'
+end
+
+post '/guess' do
+  game = get_game
+  letter = params[:guess].to_s[0]
+  begin
+    game.guess(letter)
+    session[:level] += 1 if game.won?
+  rescue ArgumentError
+    @error = 'Invalid guess.'
+  end
+  redirect '/'
+end
+
+post '/hint' do
+  game = get_game
+  session[:hint] = generate_hint(game.word, game.guesses)
+  redirect '/'
+end
+
